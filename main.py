15ddddac7d209903c80427db3fda77d701c9b209
Abhishek <shinganabhishek@gmail.com>
2024-12-21 16:14:13 +0530

    fixstcloud

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# __import__('pysqlite3')
-# import sys
+__import__('pysqlite3')
+import sys
 
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 
--- portfolio.py
+++ portfolio.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import chromadb
 import uuid
-# __import__('pysqlite3')
-# import sys
+__import__('pysqlite3')
+import sys
 
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 class Portfolio:
     def __init__(self, file_path="resources/my_portfolio.csv"):
